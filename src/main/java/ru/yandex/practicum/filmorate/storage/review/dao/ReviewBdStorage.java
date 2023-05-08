c91f25095e70d7547a46e07b4bd6c5d3bae5b5d1
Bitpr <Bitproton@yandex.ru>
2023-05-08 14:14:29 +0300

    Merge remote-tracking branch 'origin/develop' into develop

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/dao/ReviewBdStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/dao/ReviewBdStorage.java
@@ -79,12 +79,9 @@ public class ReviewBdStorage implements ReviewStorage {
     @Override
     public Optional<Review> findById(int id) {
         SqlRowSet reviewRows = jdbcTemplate.queryForRowSet("SELECT r.*, " +
-                "(COUNT(LRT.USER_ID) - COUNT(LRF.USER_ID)) AS USE " +
-                "FROM REVIEWS AS r " +
-                "LEFT JOIN (SELECT * FROM LIKE_REVIEW WHERE IS_POSITIVE = true) LRT on r.REVIEW_ID = LRT.REVIEW_ID " +
-                "LEFT JOIN (SELECT * FROM LIKE_REVIEW WHERE IS_POSITIVE = false) LRF on r.REVIEW_ID = LRF.REVIEW_ID " +
+                "SUM(CASE WHEN LR.IS_POSITIVE = TRUE THEN 1 WHEN LR.IS_POSITIVE = FALSE THEN -1 ELSE 0 END) AS USE " +
+                "FROM REVIEWS AS r " + "LEFT JOIN LIKE_REVIEW as LR on r.REVIEW_ID = LR.REVIEW_ID " +
                 " WHERE r.REVIEW_ID = ? GROUP BY r.REVIEW_ID", id);
-
         if (reviewRows.next()) {
             return Optional.of(reviewRows(reviewRows));
         } else log.info("Фильм с идентификатором {} не найден.", id);
@@ -94,18 +91,13 @@ public class ReviewBdStorage implements ReviewStorage {
     @Override
     public List<Review> findAll(int filmId, int count) {
         String where = "";
-        if (filmId != 0)
-            where = "WHERE FILM_ID = " + filmId;
-
-        String sql = "SELECT r.*, (COUNT(LRT.USER_ID) - COUNT(LRF.USER_ID)) AS USE " +
+        if (filmId != 0) where = "WHERE FILM_ID = " + filmId;
+        String sql = "SELECT r.*, " +
+                "SUM(CASE WHEN LR.IS_POSITIVE = TRUE THEN 1 WHEN LR.IS_POSITIVE = FALSE THEN -1 ELSE 0 END) AS USE " +
                 "FROM REVIEWS AS r " +
-                "LEFT JOIN (SELECT * FROM LIKE_REVIEW WHERE IS_POSITIVE = true) LRT on r.REVIEW_ID = LRT.REVIEW_ID " +
-                "LEFT JOIN (SELECT * FROM LIKE_REVIEW WHERE IS_POSITIVE = false) LRF on r.REVIEW_ID = LRF.REVIEW_ID " +
-                where +
-                " GROUP BY r.REVIEW_ID " +
-                " ORDER BY COUNT(LRT.USER_ID) - COUNT(LRF.USER_ID) DESC" +
+                "LEFT JOIN LIKE_REVIEW as LR on r.REVIEW_ID = LR.REVIEW_ID " + where +
+                " GROUP BY r.REVIEW_ID " + " ORDER BY USE DESC" +
                 " LIMIT " + count;
-
         return jdbcTemplate.query(sql, (rs, rowNum) -> makeReview(rs));
     }
 
